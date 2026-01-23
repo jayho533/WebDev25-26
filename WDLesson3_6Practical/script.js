56ff8ee89f53d7e1bd5d788b2372afb9555c9367
jayho533 <jayh3090@nycstudents.net>
2026-01-23 17:58:21 +0000

    3.6

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -1,5 +1,10 @@
 // Challenge 2:  Create the function for the Average program.  Refer to the resource link in the HTML for assistance.
-
+if not numbers_list:
+        return 0
+    total_sum = sum(numbers_list)
+    count = len(numbers_list)
+    average = total_sum / count
+    return average
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
 
