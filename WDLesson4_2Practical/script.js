372d5675a973ccb0887897a2e4f6ff0213d75396
jayho533 <jayh3090@nycstudents.net>
2026-02-09 22:02:19 +0000

    kykyky

--- WDLesson4_2Practical/script.js
+++ WDLesson4_2Practical/script.js
@@ -13,7 +13,9 @@ function name(){
 */
 
 /* Challenge 2:  Create the function for Challenge 1 scenario */
-
+function area(){
+  let
+}
 
 
 /* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
@@ -21,5 +23,28 @@ function name(){
 2) You will need to write HTML as part of the output.  
    For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
 function pet(){
+let a = document.getElementById("animal").value;
+let e = document.getElementById("emotion").value;
+let output = document.getElementById("output");
+let filename ="";
+if (a == "Bear" && e =="funny"){
+  filename ="funnyBear.jpg";
+}
+if (a == "Bear" && e =="sad"){
+  filename ="sadBear.jpg";
+}
+if (a == "cat" && e =="funny"){
+  filename ="funnycat.jpg";
+}
+if (a == "cat" && e =="sad"){
+  filename ="sadcat.jpg";
+}
+if (a == "Dog" && e =="funny"){
+  filename ="funnyDogjpg";
+}
 
+if (a == "Dog" && e =="sad"){
+  filename ="sadDogjpg";
+}
+output.innerHTML =`<img src ="${filename}">`;
 }
