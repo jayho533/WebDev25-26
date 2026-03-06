189485b861aa5bed8dff6bd4f9a9bbe7ba4a94a0
jayho533 <jayh3090@nycstudents.net>
2026-03-06 18:35:47 +0000

    5.1

---
+++ WDLesson5_1Practical/script.js
@@ -0,0 +1,23 @@
+/* Challenge 2: Create the function that will calculate the balance in an account using compound 
+interest after t number of years.
+
+Notes:
+1) Don't let the formula scare you.  Simply translate it into JS
+2) Create a for loop where the loop variable is t and the condition ends at the number of years 
+the user specifies in the text input.
+3) Use a build variable to create an output that looks like this
+Year 1: $xxxxxx
+Year 2: $xxxxxx
+Year 3: $xxxxxx
+...
+Year N: $xxxxxx
+*/
+function calculateValue(){
+    let P = parseFloat(document.getElementById("P")).value
+    let r = parseFloat(document.getElementById("r")).value
+    let n = parseInt(document.getElementById("n")).value
+    let years = parseint(document.getElementById("t").value);
+    let build =`<h2>Yearly Balances</h2>`;
+
+}
+
